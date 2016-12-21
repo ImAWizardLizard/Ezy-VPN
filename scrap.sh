cb5d6166bc026305bafa20ef61d39f2da8b39d27
Harry Newton <hazzaprime@gmail.com>
2016-12-22 00:07:48 +1000

    using curl instead of wget

--- scrap.sh
+++ scrap.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-# changer dir
-#cd /tmp
+# changer dir for temporary file useage
+cd /tmp
 
 website='http://www.vpnbook.com'
 webpage="$website/freevpn"
@@ -31,19 +31,13 @@ done
 # download html
 wget -q -nv -O $vpn_file $webpage
 
-
-
 username=$(xmllint --html --xpath '(//li[contains(text(),"Username")])[2]/strong/text()' \
  $vpn_file 2>/dev/null )
-
-
 
 password=$(xmllint --html --xpath \
   '(//li[contains(text(),"Password")])[2]/strong/text()' $vpn_file 2>/dev/null )
 
-
 echo -e "USE THESE CREDENTIALS\n\nUsername: ${username}\nPassword: ${password}"
-
 
 vpns=$(xmllint --html --xpath \
   '(//ul[contains(@class,"disc")])[2]//li/a/@href' \
@@ -60,7 +54,8 @@ do
       file="EUROPE1.zip"
       echo -e "Connecting to the EUROPE1 server"
       #wget -q -nv -O $file $website${urls[0]} && unzip -qq -d "EUROPE1" $file
-      wget -q -nv -O $file $website${urls[0]} && unzip -qq $file 
+      #wget -q -nv -O $file $website${urls[0]} && unzip -qq $file 
+      curl -s -o $file $website${urls[0]} && unzip -qq -d "EUROPE1" $file
       cd EUROPE1/
       choose_vpn
       break
@@ -69,7 +64,8 @@ do
       file="EUROPE2.zip"
       echo -e "Connecting to the EUROPE2 server"
       #wget -q -nv -O $file $website${urls[1]} && unzip -qq -d "EUROPE2" $file
-      wget -q -nv -O $file $website${urls[1]} && unzip -qq $file
+      #wget -q -nv -O $file $website${urls[1]} && unzip -qq $file
+      curl -s -o $file $website${urls[1]} && unzip -qq -d "EUROPE2" $file
       cd EUROPE2/
       choose_vpn
       break
@@ -78,7 +74,8 @@ do
       file="USA1.zip"
       echo -e "Connecting to the USA1 server"
       #wget -q -nv -O $file $website${urls[2]} && unzip -qq -d "USA1" $file
-      wget -q -nv -O $file $website${urls[2]} && unzip -qq $file
+      #wget -q -nv -O $file $website${urls[2]} && unzip -qq $file
+      curl -s -o $file $website${urls[2]} && unzip -qq -d "USA1" $file
       cd USA1/
       choose_vpn
       break
@@ -87,7 +84,8 @@ do
       file="USA2.zip"
       echo -e "Connecting to the USA2 server"
       #wget -q -nv -O $file $website${urls[3]} && unzip -qq -d "USA2" $file
-      wget -q -nv -O $file $website${urls[3]} && unzip -qq $file
+      #wget -q -nv -O $file $website${urls[3]} && unzip -qq $file
+      curl -s -o $file $website${urls[3]} && unzip -qq -d "USA2" $file
       cd USA2/
       choose_vpn
       break
@@ -96,7 +94,8 @@ do
       file="CANADA.zip"
       echo -e "Connecting to the CANADA server"
       #wget -q -nv -O $file $website${urls[4]} && unzip -qq -d "CANADA" $file
-      wget -q -nv -O $file $website${urls[4]} && unzip -qq $file
+      #wget -q -nv -O $file $website${urls[4]} && unzip -qq $file
+      curl -s -o $file $website${urls[4]} && unzip -qq -d "CANADA" $file
       cd CANADA/
       choose_vpn
       break
@@ -105,7 +104,8 @@ do
       file="GERMANY.zip"
       echo -e "Connecting to the GERMANY server"
       #wget -q -nv -O $file $website${urls[5]} && unzip -qq -d "GERMANY" $file
-      wget -q -nv -O $file $website${urls[5]} && unzip -qq $file
+      #wget -q -nv -O $file $website${urls[5]} && unzip -qq $file
+      curl -s -o $file $website${urls[5]} && unzip -qq -d "GERMANY" $file
       cd GERMANY/
       choose_vpn
       break
